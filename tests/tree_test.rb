847c226d959c9faa6c6ee6a42592e129b7911480
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-28 20:29:27 -0500

    Wrote tests for tree

--- tests/tree_test.rb
+++ tests/tree_test.rb
@@ -1,2 +1,43 @@
 require_relative "test_helper"
 require_relative "../lib/tree.rb"
+
+class TreeTest < Minitest::Test
+	# :species, common_name:, :lifespan, :type, :average_height
+	def test_can_be_created_under_normal_circumstances
+    tree = Tree.new(species: "Sequoia sempervirens", common_name: "coast redwood", 
+    	lifespan: 650, type: "evergreen", average_height: 112)
+    refute_nil(tree)
+  end
+	
+	def test_species_getter
+    tree = Tree.new(species: "Sequoia sempervirens", common_name: "coast redwood", 
+    	lifespan: 650, type: "evergreen", average_height: 112)
+    assert_equal("Sequoia sempervirens", tree.species)
+  end
+
+  def test_common_name_getter
+    tree = Tree.new(species: "Sequoia sempervirens", common_name: "coast redwood", 
+    	lifespan: 650, type: "evergreen", average_height: 112)
+    assert_equal("coast redwood", tree.common_name)
+  end
+
+
+  def test_lifespan_getter
+    tree = Tree.new(species: "Sequoia sempervirens", common_name: "coast redwood", 
+    	lifespan: 650, type: "evergreen", average_height: 112)
+    assert_equal(650, tree.lifespan)
+  end
+
+  def test_type_getter
+    tree = Tree.new(species: "Sequoia sempervirens", common_name: "coast redwood", 
+    	lifespan: 650, type: "evergreen", average_height: 112)
+    assert_equal("evergreen", tree.type)
+  end
+  
+  def test_average_height_getter
+    tree = Tree.new(species: "Sequoia sempervirens", common_name: "coast redwood", 
+    	lifespan: 650, type: "evergreen", average_height: 112)
+    assert_equal(112, tree.average_height)
+  end
+
+end
