679e1fbbfb96e015b9ce38e671f3ac84367ad9d5
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-28 20:25:50 -0500

    Tree inherits from ActiveRecord now

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,13 +1,8 @@
 require 'pry'
+require 'active_record'
 
-class Tree
-	attr_accessor :species, :common_name, :lifespan, :type, :average_height
+class Tree < ActiveRecord::Base
 
-	def initialize(species:, common_name:, lifespan:, type:, average_height:)
-		@species = species
-		@common_name = common_name
-		@lifespan = lifespan
-		@type = type
-		@average_height = average_height
-	end
 end
+
+binding.pry
